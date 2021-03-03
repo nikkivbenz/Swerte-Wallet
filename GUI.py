e1ba75b3a126b9c01e388c76ec2abbfe25bfae17
Nikki Benitez <39257026+nikkivbenz@users.noreply.github.com>
2021-03-02 17:01:41 -0800

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -1,12 +1,31 @@
-
-#imports everything tkinter has to offer 
 from tkinter import *
 
 
+#root window
 root = Tk()
+root.title("Swerte Wallet")
+root.geometry('350x200')
 
-a = Label(root, text = "Hello World")
-a.pack()
 
+#menu @ top
+menu = Menu(root)
+item = Menu(menu)
+
+menu.add_cascade(label = 'File', menu = item)
+item.add_command(label = 'New')
+root.config(menu = menu)
+
+def startWindow(): 
+    startMenu = Label(root, text = "Welcome to Swerte Wallet")
+    startMenu.grid()    
+
+def clicked(): 
+    pass
+
+
+
+
+
+startWindow()
 root.mainloop()
 
