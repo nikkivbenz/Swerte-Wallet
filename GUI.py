3207b5a79b510ef06034146bc3aca90fec9ab547
Nikki Benitez <39257026+nikkivbenz@users.noreply.github.com>
2021-03-02 17:17:20 -0800

    Update GUI.py
    Created 3 buttons for the start window

--- GUI.py
+++ GUI.py
@@ -8,19 +8,31 @@ root.geometry('350x200')
 
 
 #menu @ top
-menu = Menu(root)
-item = Menu(menu)
-
-menu.add_cascade(label = 'File', menu = item)
-item.add_command(label = 'New')
-root.config(menu = menu)
 
 def startWindow(): 
     startMenu = Label(root, text = "Welcome to Swerte Wallet")
-    startMenu.grid()    
+    startMenu.grid(row = 0, column = 1)    
+    createAccountButton = Button(root, text = "Create a XLM Account", command = createAccount)
+    createAccountButton.grid(row = 2, column = 1)
 
+    reloadAccountButton = Button(root, text = "Load an existing XLM Account", command = loadAccount)
+    reloadAccountButton.grid(row = 4, column = 1)
+
+    learnMoreButton = Button(root, text = "Learn more about Swerte Wallet", command = learnMorePage)
+    learnMoreButton.grid(row = 6, column = 1)
+
+
+def learnMorePage(): 
+    pass
+def createAccount():
+    pass
+
+def loadAccount(): 
+    pass
 def clicked(): 
     pass
+
+
 
 
 
