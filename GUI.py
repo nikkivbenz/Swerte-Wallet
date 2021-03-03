c5f6fdf240de0ad104e4e7e9feb5e7dbe00449ae
Nikki Benitez <39257026+nikkivbenz@users.noreply.github.com>
2021-03-02 20:50:45 -0800

    Update GUI.py
    Added Main Page and Learn More Page

--- GUI.py
+++ GUI.py
@@ -1,29 +1,47 @@
 from tkinter import *
 
+#tkk? 
 
-#root window
+#base for all frames
 root = Tk()
 root.title("Swerte Wallet")
 root.geometry('350x200')
 
+startWindowFrame = Frame(root)     
+learnMoreFrame = Frame(root)
 
-#menu @ top
 
 def startWindow(): 
-    startMenu = Label(root, text = "Welcome to Swerte Wallet")
-    startMenu.grid(row = 0, column = 1)    
-    createAccountButton = Button(root, text = "Create a XLM Account", command = createAccount)
-    createAccountButton.grid(row = 2, column = 1)
+    learnMoreFrame.pack_forget()
+    startWindowFrame.tkraise()
+    welcomeText = Label(startWindowFrame, text = "Welcome to Swerte Wallet")
+    welcomeText.grid(row = 0, column = 2)    
 
-    reloadAccountButton = Button(root, text = "Load an existing XLM Account", command = loadAccount)
-    reloadAccountButton.grid(row = 4, column = 1)
+    createAccountButton = Button(startWindowFrame, text = "Create a XLM Account", command = createAccount)
+    createAccountButton.grid(row = 2, column = 2)
 
-    learnMoreButton = Button(root, text = "Learn more about Swerte Wallet", command = learnMorePage)
-    learnMoreButton.grid(row = 6, column = 1)
+    reloadAccountButton = Button(startWindowFrame, text = "Load an existing XLM Account", command = loadAccount)
+    reloadAccountButton.grid(row = 4, column = 2)
 
+    learnMoreButton = Button(startWindowFrame, text = "Learn more about Swerte Wallet", command = learnMore)
+    learnMoreButton.grid(row = 6, column = 2)
+    startWindowFrame.pack()
+    
 
-def learnMorePage(): 
-    pass
+def learnMore(): 
+    startWindowFrame.pack_forget()
+    learnMoreFrame.tkraise()
+    aboutXLMText = Label(learnMoreFrame, text = "This wallet supports the storage of XLM from the Stellar Lumens Foundation")
+    aboutXLMText.grid(row = 0, column  = 2)
+
+    functionalitiesText= Label(learnMoreFrame, text = "Here, you can create a new Stellar Lumens account or load up an existing one.")
+    functionalitiesText.grid(row = 3, column = 2) 
+
+    backToStartButton = Button(learnMoreFrame, text = "Back to Main Page", command = startWindow)
+    backToStartButton.grid(row = 5, column = 2) 
+    learnMoreFrame.pack()
+    
+
 def createAccount():
     pass
 
@@ -33,11 +51,5 @@ def clicked():
     pass
 
 
-
-
-
-
-
 startWindow()
 root.mainloop()
-
